449bbd36dc59850132f9ccce7072285ce6907e69
Алексей Небензя <aleksei6191@gmail.com>
2025-03-22 21:28:00 +0300

    Add App

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CopyPasteApi.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private static string _storedString = string.Empty;
+
+        [HttpGet("paste")]
+        public IActionResult Get()
+        {
+            return Ok(_storedString);
+        }
+
+        [HttpPut("copy")]
+        public IActionResult Put([FromBody] string newValue)
+        {
+            if (string.IsNullOrWhiteSpace(newValue))
+            {
+                return BadRequest("Значение не должно быть пустым.");
+            }
+
+            _storedString = newValue;
+            return Ok($"Новое значение установлено: {_storedString}");
+        }
+    }
+
+}
+
